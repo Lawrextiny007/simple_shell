c15fac03fda3217e91c8f3dfb775076cca420631
Lawrextiny007 <lawrencejimmy884@gmail.com>
2023-02-18 02:56:17 -0800

    betty check

---
+++ betty.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+
+/**
+ * main - add sum of numbers
+ * @num: first integer to be added
+ * @num1: second integer to be added
+ *
+ * Return: (0) success
+ */
+
+int main(int num, int num1)
+{
+	int sum;
+
+	num = 5;
+	num1 = 7;
+	sum = num + num1;
+
+	printf("%d", sum);
+	return (0);
+}
+
