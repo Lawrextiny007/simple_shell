80dae37815c93c36c876331dbde18ad13383975a
Lawrextiny007 <lawrencejimmy884@gmail.com>
2023-02-21 22:31:10 -0800

    simple shell project

---
+++ betty.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+
+/**
+ * add - sum of two digits
+ * @num: first integer
+ * @num1: second integer
+ *
+ * Return: (0) success
+ */
+
+int add(int num, int num1)
+{
+	int sum;
+
+	num = 5;
+	num1 = 7;
+	sum = num + num1;
+
+	printf("%d", sum);
+	return (0);
+}
